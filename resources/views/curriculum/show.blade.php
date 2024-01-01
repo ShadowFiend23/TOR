525bb9a1e68fe4225360f707201c2ab9052c38af
Pudding(Lloyd) <89757149+KiyoScript@users.noreply.github.com>
2024-01-01 13:21:47 +0800

    [TOR] Curriculum Edit Page Continuation UI (#17)
    https://github.com/ShadowFiend23/TOR/issues/16

--- resources/views/curriculum/show.blade.php
+++ resources/views/curriculum/show.blade.php
@@ -128,6 +128,7 @@
                         <option selected>Semester</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
+                        <option selected>Semester</option>
                       </select>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::get('/course/new', function () { return view('course/new'); });
 # Curriculum
 Route::get('/curriculum', function () { return view('curriculum/index'); });
 Route::get('/curriculum/show', function () { return view('curriculum/show'); });
+Route::get('/curriculum/edit', function () { return view('curriculum/edit'); });
 
 Route::get('/studentLogin',[StudentLoginController::class,'show'])->middleware('guest')->name('studentLogin');
 Route::post('/studentLogin',[StudentLoginController::class,'login'])->middleware('guest');
