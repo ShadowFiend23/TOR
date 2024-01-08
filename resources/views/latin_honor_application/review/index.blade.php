2bbcaaeba889bbd33c6003474a7af84189224a42
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-01-08 22:06:49 +0800

    [TOR] Latin Honor Application User Interface Page
    Resolves: https://github.com/ShadowFiend23/TOR/issues/29
    
    @akym-03 @rosfloresdelan

---
+++ resources/views/latin_honor_application/review/index.blade.php
@@ -0,0 +1,232 @@
+<x-authenticated_layout>
+  <div class="container-fluid">
+
+
+    <div class="modal fade" id="deniedModal" tabindex="-1" aria-labelledby="deniedModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-body">
+            <h5>Confirmation</h5>
+            <p class="text-black">
+              The evaluation for this student has been denied, as the records did not meet the Latin honors criteria.
+            </p>
+            <div class="text-end">
+              <button type="button" class="btn btn-info btn-sm text-white" data-coreui-dismiss="modal">Ok</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="d-flex flex-row align-items-center justify-content-end mb-5 gap-2 pt-0 m-0">
+      <button type="button" class="btn btn-primary text-white mt-3" data-coreui-toggle="modal" data-coreui-target="#deniedModal">
+        Deny
+      </button>
+      <a href="../../latin_honor_application/" class="btn btn-success text-white mt-3">Approve</a>
+    </div>
+    <div class="accordion" id="accordionExample">
+      <div class="accordion-item text-white">
+        <h2 class="accordion-header">
+          <button class="accordion-button bg-dark text-white" type="button" data-coreui-toggle="collapse" data-coreui-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            <span class="fs-4 me-2"><i class='bx bx-list-ul' ></i></span>
+                Course : OC BSIT,
+                  AY : 2021-2022,
+                  Sem : 1
+          </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" data-coreui-parent="#accordionExample">
+          <div class="accordion-body">
+            <div class="panel-body" style="display: block;">
+              <div class="table-responsive">
+                <table class="table ">
+                  <tr>
+                      <th style="width: 5%">Grade</th>
+                      <th style="width: 5%">Comp.</th>
+                      <th style="width: 15%">Code</th>
+                      <th style="width: 7%">Sec.</th>
+                      <th >Description</th>
+                  </tr>
+                  <tr>
+                    <td> 1.5 </td>
+                    <td>  </td>
+                    <td> IT 134 </td>
+                    <td> 1A </td>
+                    <td> Computer Programming 1 </td>
+                  </tr>
+                  <tr>
+                    <td> 1.1 </td>
+                    <td>  </td>
+                    <td> GEN ED 001 </td>
+                    <td> 1A </td>
+                    <td> Purposive Communication </td>
+                  </tr>
+                  <tr>
+                    <td> 2.0 </td>
+                    <td>  </td>
+                    <td> MATH ENHANCE 1 </td>
+                    <td> 1A </td>
+                    <td> College Algebra and Trigonometry </td>
+                  </tr>
+                  <tr>
+                    <td> 1.9 </td>
+                    <td>  </td>
+                    <td> GEN ED 004 </td>
+                    <td> 1A </td>
+                    <td> Mathematics in the Modern world </td>
+                  </tr>
+                  <tr>
+                    <td> 1.1 </td>
+                    <td>  </td>
+                    <td> GEN ED 009 </td>
+                    <td> 1A </td>
+                    <td> The Entrepreneurial Mind </td>
+                  </tr>
+                  <tr>
+                    <td> 1.6 </td>
+                    <td>  </td>
+                    <td> IT 113 </td>
+                    <td> 1A </td>
+                    <td> Introduction to Computing </td>
+                  </tr>
+                  <tr>
+                    <td> 1.2 </td>
+                    <td>  </td>
+                    <td> NSTP 113 </td>
+                    <td> 1A </td>
+                    <td> CWTS, LTS, MTS(Naval or Air Force) </td>
+                  </tr>
+                  <tr>
+                    <td> 1.4 </td>
+                    <td>  </td>
+                    <td> DRR 113 </td>
+                    <td> 1A </td>
+                    <td> Disaster Risk Reduction and Education in Emergencies </td>
+                  </tr>
+                  <tr>
+                    <td> 2.2 </td>
+                    <td>  </td>
+                    <td> GEN ED 002 </td>
+                    <td> 1A </td>
+                    <td> Understanding the Self </td>
+                  </tr>
+                  <tr>
+                    <td> 1.0 </td>
+                    <td>  </td>
+                    <td> PATHFIT 112 </td>
+                    <td> 1A </td>
+                    <td> Movement Competency Training </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="accordion" id="accordionExample">
+      <div class="accordion-item text-white">
+        <h2 class="accordion-header">
+          <button class="accordion-button bg-dark text-white" type="button" data-coreui-toggle="collapse" data-coreui-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            <span class="fs-4 me-2"><i class='bx bx-list-ul' ></i></span>
+                Course : OC BSIT,
+                  AY : 2021-2022,
+                  Sem : 1
+          </button>
+        </h2>
+        <div id="collapseOne" class="accordion-collapse collapse show" data-coreui-parent="#accordionExample">
+          <div class="accordion-body">
+            <div class="panel-body" style="display: block;">
+              <div class="table-responsive">
+                <table class="table ">
+                  <tr>
+                      <th style="width: 5%">Grade</th>
+                      <th style="width: 5%">Comp.</th>
+                      <th style="width: 15%">Code</th>
+                      <th style="width: 7%">Sec.</th>
+                      <th >Description</th>
+                  </tr>
+                  <tr>
+                    <td> 1.5 </td>
+                    <td>  </td>
+                    <td> IT 134 </td>
+                    <td> 1A </td>
+                    <td> Computer Programming 1 </td>
+                  </tr>
+                  <tr>
+                    <td> 1.1 </td>
+                    <td>  </td>
+                    <td> GEN ED 001 </td>
+                    <td> 1A </td>
+                    <td> Purposive Communication </td>
+                  </tr>
+                  <tr>
+                    <td> 2.0 </td>
+                    <td>  </td>
+                    <td> MATH ENHANCE 1 </td>
+                    <td> 1A </td>
+                    <td> College Algebra and Trigonometry </td>
+                  </tr>
+                  <tr>
+                    <td> 1.9 </td>
+                    <td>  </td>
+                    <td> GEN ED 004 </td>
+                    <td> 1A </td>
+                    <td> Mathematics in the Modern world </td>
+                  </tr>
+                  <tr>
+                    <td> 1.1 </td>
+                    <td>  </td>
+                    <td> GEN ED 009 </td>
+                    <td> 1A </td>
+                    <td> The Entrepreneurial Mind </td>
+                  </tr>
+                  <tr>
+                    <td> 1.6 </td>
+                    <td>  </td>
+                    <td> IT 113 </td>
+                    <td> 1A </td>
+                    <td> Introduction to Computing </td>
+                  </tr>
+                  <tr>
+                    <td> 1.2 </td>
+                    <td>  </td>
+                    <td> NSTP 113 </td>
+                    <td> 1A </td>
+                    <td> CWTS, LTS, MTS(Naval or Air Force) </td>
+                  </tr>
+                  <tr>
+                    <td> 1.4 </td>
+                    <td>  </td>
+                    <td> DRR 113 </td>
+                    <td> 1A </td>
+                    <td> Disaster Risk Reduction and Education in Emergencies </td>
+                  </tr>
+                  <tr>
+                    <td> 2.2 </td>
+                    <td>  </td>
+                    <td> GEN ED 002 </td>
+                    <td> 1A </td>
+                    <td> Understanding the Self </td>
+                  </tr>
+                  <tr>
+                    <td> 1.0 </td>
+                    <td>  </td>
+                    <td> PATHFIT 112 </td>
+                    <td> 1A </td>
+                    <td> Movement Competency Training </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container p-5" style="width:20rem;">
+      <div class="border border-2 d-flex text-center flex-column p-4 bg-body-secondary">
+        <strong>GWA</strong>
+        <h4>1.2</h4>
+      </div>
+    </div>
+  </div>
+</x-authenticated_layout>
